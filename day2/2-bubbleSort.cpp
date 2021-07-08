1deeca3471732284f95a220591783873c2c17672
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-07-08 19:23:38 +0200

    oops, forgot another loop

--- day2/2-bubbleSort.cpp
+++ day2/2-bubbleSort.cpp
@@ -1,6 +1,6 @@
 /**
  * Day 2 - Bubble sort
- * 
+ *
  * Time complexity O(n**2)
  */
 
@@ -14,24 +14,25 @@ using namespace std;
 
 class Solution
 {
-public:
+ public:
   template <typename T>
   void solve(vector<T> arr)
   {
-    cout << endl
-         << "Original: " << arrayStringify(arr) << endl;
+    cout << endl << "Original: " << arrayStringify(arr) << endl;
     int n = arr.size();
-    for (size_t i = 1; i < n; ++i)
+    for (size_t i = 0; i < n; ++i)
     {
-      if (arr.at(i) < arr.at(i - 1))
+      for (size_t j = 1; j < n; ++j)
       {
-        T temp = arr.at(i);
-        arr[i] = arr.at(i - 1);
-        arr[i - 1] = temp;
+        if (arr.at(j) < arr.at(j - 1))
+        {
+          T temp = arr.at(j);
+          arr[j] = arr.at(j - 1);
+          arr[j - 1] = temp;
+        }
       }
     }
-    cout << "Sorted: " << arrayStringify(arr) << endl
-         << endl;
+    cout << "Sorted: " << arrayStringify(arr) << endl << endl;
   }
 };
 
@@ -39,19 +40,48 @@ int main(void)
 {
   Solution solution = Solution();
   const vector<string> arr0 = {"apollo", "patreon", "hello", "typescript"};
-  const vector<string> arr1 = {"somewhere", "influence", "about", "announced", "fresh", "compound"
-                                                                                        "cloth",
-                               "tie", "sucks", "twice", "major", "phrase"
-                                                                 "cause",
-                               "public", "lucky", "planned", "essential", "quarter"
-                                                                          "chapter",
-                               "money", "animal", "with", "equally", "refer"
-                                                                     "verb",
-                               "tonight", "shape", "cause", "putting", "widely"
-                                                                       "cave",
-                               "gather", "dry", "shout", "wonderful", "speed"
-                                                                      "each",
-                               "comfortable", "list", "opinion", "dropped", "bark"};
+  const vector<string> arr1 = {"somewhere",
+                               "influence",
+                               "about",
+                               "announced",
+                               "fresh",
+                               "compound"
+                               "cloth",
+                               "tie",
+                               "sucks",
+                               "twice",
+                               "major",
+                               "phrase"
+                               "cause",
+                               "public",
+                               "lucky",
+                               "planned",
+                               "essential",
+                               "quarter"
+                               "chapter",
+                               "money",
+                               "animal",
+                               "with",
+                               "equally",
+                               "refer"
+                               "verb",
+                               "tonight",
+                               "shape",
+                               "cause",
+                               "putting",
+                               "widely"
+                               "cave",
+                               "gather",
+                               "dry",
+                               "shout",
+                               "wonderful",
+                               "speed"
+                               "each",
+                               "comfortable",
+                               "list",
+                               "opinion",
+                               "dropped",
+                               "bark"};
   const vector<double> arr2 = {3.4, 1.9, 54.8, 30.6, 77.55};
   solution.solve(arr0);
   solution.solve(arr1);
