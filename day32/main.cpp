ed5e021da23d29d4c094b99cc33f87a21596f7a0
Tim <harvard90873@gmail.com>
2021-08-09 13:35:00 +0200

    day32

---
+++ day32/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+/*
+ * Definition for a binary tree node. */
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right)
+  {
+  }
+};
+class Solution
+{
+ public:
+  vector<int> inorderTraversal(TreeNode *root)
+  {
+    vector<int> res = {};
+
+    recurse(root, &res);
+
+    return res;
+  }
+
+  void recurse(TreeNode *node, vector<int> *vals)
+  {
+    if (node == nullptr)
+    {
+      return;
+    }
+    recurse(node->left, vals);
+    vals->push_back(node->val);
+    recurse(node->right, vals);
+  }
+};
