30229acc31599824d0046062c424ea68d1a970fd
Tim <harvard90873@gmail.com>
2021-08-15 16:31:59 +0200

    day43

---
+++ day43/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class Node
+{
+ public:
+  int val;
+  vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val) { val = _val; }
+
+  Node(int _val, vector<Node *> _children)
+  {
+    val = _val;
+    children = _children;
+  }
+};
+
+class Solution
+{
+ public:
+  int maxDepth(Node *root)
+  {
+    if (!root) return 0;
+
+    queue<Node *> q;
+
+    q.push(root);
+
+    int res = 0;
+
+    while (!q.empty())
+    {
+      for (int i = 0, n = q.size(); i < n; ++i)
+      {
+        Node *curr = q.front();
+        q.pop();
+
+        for (Node *child : curr->children)
+        {
+          q.push(child);
+        }
+      }
+
+      res++;
+    }
+
+    return res;
+  }
+};
