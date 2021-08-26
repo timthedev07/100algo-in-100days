3e814f6c22fc7e6e05a8764b2e4cc80aa129566c
Tim <harvard90873@gmail.com>
2021-08-26 17:35:20 +0200

    some comments

--- day50/main.cpp
+++ day50/main.cpp
@@ -19,9 +19,14 @@ class Solution
 
     for (int i = 0, n = nums.size(); i < n; ++i)
     {
+      // if element already exists i.e. nums[i] == nums[j] constraint satisfied
       if (map.count(nums[i]))
       {
+        // if the absolute different between i and the existing element's index
+        // is less than k
         if (abs(map[nums[i]] - i) <= k) return true;
+
+        // else(difference is larger than k), update position
         map[nums[i]] = i;
       }
       else
