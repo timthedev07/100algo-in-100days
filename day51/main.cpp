ba016c0c8c9f0a3b522056a33cfd8f99fa059026
Tim <harvard90873@gmail.com>
2021-08-27 19:25:50 +0200

    linear solution using kadane's algorithm

--- day51/main.cpp
+++ day51/main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 class Solution
 {
+  int maxSubArray(vector<int> &arr)
+  {
+    int best = arr[0];
+
+    int curr = arr[0];
+
+    for (int i = 1, n = arr.size(); i < n; ++i)
+    {
+      // if adding the current item to the existing sub array
+      // would resulter in a greater sum than starting a new sub array from the
+      // current item set best to be curr + arr[i]
+      curr = max(curr + arr[i], arr[i]);
+      best = max(curr, best);
+    }
+    return best;
+  }
   int bruteForce(vector<int> arr)
   {
     int best = numeric_limits<int>::min();
