7a1fb7eeeceadb84302b9ede981ac114ef6f16e7
Tim <harvard90873@gmail.com>
2021-09-07 19:12:42 +0200

    day 62

---
+++ day62/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * Given an integer array nums and an integer k, return the kth largest element
+ * in the array.
+ * Note that it is the kth largest element in the sorted order, not the kth
+ * distinct element.
+ */
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+ public:
+  int findKthLargest(vector<int> &nums, int k)
+  {
+    sort(nums.begin(), nums.end(), greater<int>());
+
+    return nums[k - 1];
+  }
+};
